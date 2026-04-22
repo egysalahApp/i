d5ca6e04a682ecdfdd3f35d099b3d72c7d049b02
egysalahApp <egysalah@gmail.com>
2026-04-22 17:54:00 +0300

    chore: remove test_lesson from lessons registry and source files

--- src/lessons/index.js
+++ src/lessons/index.js
@@ -1,9 +1,7 @@
 import { APP_DATA as sarfe3rabData } from "./sarfe3rab";
 import { APP_DATA as ayam15_17Data } from "./ayam15-17";
-import { APP_DATA as testLessonData } from "./test_lesson";
 
 export const lessonsData = {
   "sarfe3rab": sarfe3rabData,
   "ayam15-17": ayam15_17Data,
-  "test_lesson": testLessonData,
 };
